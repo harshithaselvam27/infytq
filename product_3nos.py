ef11ff5b045fe5f1ac358fb7eb5087b5b52314cc
harshithaselvam27 <50586653+harshithaselvam27@users.noreply.github.com>
2019-05-31 13:30:52 +0530

    Create product_3nos.py

---
+++ product_3nos.py
@@ -0,0 +1,17 @@
+def display(num):
+    message=""
+    if num%3==0 and num%5==0:
+        message="zoom"
+    elif num%3==0:
+        message="zip"
+    elif num%5==0:
+        message="zap"
+    else:
+        message="invalid"
+        
+    #write your logic here
+    return message
+
+#Provide different values for num and test your program
+message=display(9)
+print(message)
