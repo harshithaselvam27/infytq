321f385c3d052c78faf0f8fd51e6e84ac1b8073f
harshithaselvam27 <50586653+harshithaselvam27@users.noreply.github.com>
2019-06-09 20:24:27 +0530

    Create 26_rabbit.py

---
+++ 26_rabbit.py
@@ -0,0 +1,30 @@
+def solve(heads,legs):
+    error_msg="No solution"
+    rabbit_count=0 
+    chicken_count=0 
+    rabbit_count=(legs-(heads*2))
+    if (rabbit_count>=1):
+        if(rabbit_count % 2==0):
+            rabbit_count=rabbit_count//2
+            chicken_count=abs(heads-rabbit_count)
+        if(chicken_count+rabbit_count==heads):
+            print(chicken_count,rabbit_count)
+        else:
+            print("error_msg")
+    else:
+        print(error_msg)
+
+    
+    #Start writing your code here
+    #Populate the variables: chicken_count and rabbit_count
+
+
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+
+    #print(chicken_count,rabbit_count)
+    #print(error_msg)
+
+#Provide different values for heads and legs and test your program
+solve(150,400)
